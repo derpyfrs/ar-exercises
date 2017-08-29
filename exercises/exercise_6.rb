d6bcb340502ccd8f5c8fdae6f4826a6e8e4c08db
derpyfrs <simon_chanct@hotmail.com>
2017-08-29 23:15:15 +0000

    exercise 7

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -18,9 +18,9 @@ end
 
 @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
 @store2.employees.create(first_name: "Hannah", last_name: "GoldenRetreiever", hourly_rate: 40)
-@store4.employees.create(first_name: "Edmond", last_name: "Corgi", hourly_rate: 20)
-@store5.employees.create(first_name: "Simon", last_name: "Pomsky", hourly_rate: 200)
-@store6.employees.create(first_name: "Wing", last_name: "Samoyed", hourly_rate:100)
-@store3.employees.create(first_name: "Courtney", last_name: "Poodle", hourly_rate:40)
+@store1.employees.create(first_name: "Edmond", last_name: "Corgi", hourly_rate: 20)
+@store1.employees.create(first_name: "Simon", last_name: "Pomsky", hourly_rate: 200)
+@store2.employees.create(first_name: "Wing", last_name: "Samoyed", hourly_rate:100)
+@store1.employees.create(first_name: "Courtney", last_name: "Poodle", hourly_rate:40)
 
-pp Employee.all
+# pp Employee.all
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,32 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+class Employee < ActiveRecord::Base
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+  validates_inclusion_of :hourly_rate, in: 40..200
+  validates_presence_of :store_id
+end
+
+class Store < ActiveRecord::Base
+  validates_length_of :name, minimum: 3
+  validates :annual_revenue,
+            numericality: { only_interger: true,
+              greater_than: 0}
+        end
+
+
+
+print "Gimmie a store name"
+
+@store_name = gets.chomp
+
+e = Store.create(name: @store_name, annual_revenue: nil, mens_apparel: nil, womens_apparel: nil)
+
+# pp Store.all
+
+if e.errors.any?
+  puts e.errors.full_messages
+else
+  puts "you aite"
+end
